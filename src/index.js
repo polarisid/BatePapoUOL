b3447ed366ab5a53a3b0117461a3da8df7d0d74f
polarisid <danielcaevalhorst@gmail.com>
2021-11-14 03:23:51 -0200

    Intefrace de login ok e com interface de users

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ let nome;
 let idinterval;
 let messages;
 let msgHTML= document.querySelector(".messages-container");
+let documento_person = document.querySelector(".persons-list");
 
 let dados_msg;
+let dados_msg2;
 let msgENVIAR;
+let dados_persons;
 
 
 
@@ -90,43 +93,108 @@ function carregarMsg(){//este puxa a exibir msg
 
 function postarMsg(resposta){
     dados_msg=resposta.data;
-  
-    msgHTML.innerHTML =""
+    
+    if(dados_msg2==undefined){ /// assim que entra na sala
+        dados_msg2=dados_msg
+        for(var i = 0;i<(dados_msg.length);i++){
+            if(dados_msg[i].type =="status"){
+                msgHTML.innerHTML +=
+        
+                `
+                <li class="message-text status ">
+                    <span class="hour">(${dados_msg[i].time})</span>
+                    <span class="person-name">${dados_msg[i].from }</span>
+                    <span>${dados_msg[i].text }</span>
+                </li>
+                `
+            }
+        
+            else{
+                msgHTML.innerHTML +=
+                `
+                <li class="message-text data-identifier="message"">
+                    <span class="hour">(${dados_msg[i].time})</span>
+                    <span class="person-name">${dados_msg[i].from }</span>
+                    <span>${dados_msg[i].text }</span>
+                </li>
+                `
+            }
+            }
+            let elementoQueQueroQueApareca = document.querySelector("ul li:last-child");
+            elementoQueQueroQueApareca.scrollIntoView();
+    
+    }
+    
+    if((dados_msg[99].time)!=((dados_msg2[99].time))){ //se tem atualização nas mensagens
+        console.log("tem atualização")
+        dados_msg2=dados_msg;
+        msgHTML.innerHTML =""
 
     
-    for(var i = 0;i<(dados_msg.length);i++){
-    if(dados_msg[i].type =="status"){
-        msgHTML.innerHTML +=
-
-        `
-        <li class="message-text status">
-            <span class="hour">(${dados_msg[i].time})</span>
-            <span class="person-name">${dados_msg[i].from }</span>
-            <span>${dados_msg[i].text }</span>
-        </li>
-       
+        for(var i = 0;i<(dados_msg.length);i++){
+        if(dados_msg[i].type =="status"){
+            msgHTML.innerHTML +=
     
-        `
+            `
+            <li class="message-text status ">
+                <span class="hour">(${dados_msg[i].time})</span>
+                <span class="person-name">${dados_msg[i].from }</span>
+                <span>${dados_msg[i].text }</span>
+            </li>
+           
+        
+            `
+        }
+    
+        else{
+            msgHTML.innerHTML +=
+            `
+            <li class="message-text data-identifier="message"">
+                <span class="hour">(${dados_msg[i].time})</span>
+                <span class="person-name">${dados_msg[i].from }</span>
+                <span>${dados_msg[i].text }</span>
+            </li>
+            `
+        }
+        }
+        let elementoQueQueroQueApareca = document.querySelector("ul li:last-child");
+        elementoQueQueroQueApareca.scrollIntoView();
+
+    }
+    else{ // se não tem ele não executa atuzalização nem scroll into-view
+        console.log("n tem atualização")
     }
 
-    else{
-        msgHTML.innerHTML +=
+}
 
-        `
-        <li class="message-text">
-            <span class="hour">(${dados_msg[i].time})</span>
-            <span class="person-name">${dados_msg[i].from }</span>
-            <span>${dados_msg[i].text }</span>
-        </li>
-       
+function exibeLateral(){
+    let menulateral = document.querySelector("aside");
+    menulateral.classList.toggle("escondido")
+    let promisseperson=axios.get("https://mock-api.driven.com.br/api/v4/uol/participants");
+    promisseperson.then(pegarpessoas)
+    promisseperson.catch(tratarFalha)
     
-        `
+}
+function pegarpessoas(resposta1){
+    dados_persons =resposta1.data;
+    //console.log(dados_person)
+    
+    documento_person.innerHTML="";
+
+    for(let i=0; i<(dados_persons.length);i++){
+        documento_person.innerHTML+=`
+        <li class="person-in-list">
+        <ion-icon name="person-circle"></ion-icon>
+        <p class="name-person-list">${dados_persons[i].name}</p>
+        </li>
+        `;
     }
     
+}
+function fora(){
+    let menulateral = document.querySelector("aside");
+    menulateral.classList.add("escondido")
+}
 
-    
-   
-}
-    let elementoQueQueroQueApareca = document.querySelector("ul li:last-child");
-    elementoQueQueroQueApareca.scrollIntoView();
-}
+
+
