f14cb986eecccbbe9b288872e28fe67f9d8b0c8f
polarisid <danielcaevalhorst@gmail.com>
2021-11-15 12:12:04 -0200

    recebendo Private messages e selecionando visibilidade

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
-
-
 let nome;
 let idinterval;
 let messages;
 let msgHTML= document.querySelector(".messages-container");
 let documento_person = document.querySelector(".persons-list");
-
 let dados_msg;
 let dados_msg2;
 let msgENVIAR;
 let dados_persons;
-
-
-
+let dados_nome_direct = "Todos";
+let dados_type_message = "message";
+document.querySelector(".public").click(); // para resolver o bug do selecionável na área
 
 function entry(){
     let nome1 = document.querySelector(".login-page input");
@@ -22,12 +19,6 @@ function entry(){
     enviarNome(nome);
     
 }
-
-
-
-
-//responder 400 - se já houver um user online - enquanto isso solicitar novo nome
-//status 200 - ok
 function enviarMSG(){
     msgENVIAR= document.querySelector(".input-box");
     
@@ -35,16 +26,15 @@ function enviarMSG(){
     const promisses = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", 
      {
         from: nome,
-        to: "Todos",
+        to: dados_nome_direct,
         text: msgENVIAR.value,
-        type: "message" // ou "private_message" para o bônus
+        type: dados_type_message // ou "private_message" para o bônus
     })
     promisses.then(carregarMsg)
     promisses.catch(tratarFalha)
    
     msgENVIAR.value="";
 }
-
 function enviarNome(id){
     const promessa = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants ", {name:id})
     promessa.then(manterVivo)
@@ -53,7 +43,6 @@ function enviarNome(id){
     promessa.catch(tratarFalha)
     return;
 }
-
 function tratarFalha(erro) {
 	const statusCode = erro.response.status;
 	if(statusCode=="400"){
@@ -69,28 +58,25 @@ function tratarFalha(erro) {
     //location.reload();
     window.location.reload()
 }
-
 function manterVivo(){//este puxa a estou vivo
     idinterval = setInterval(estouVivo, 5000);
+   let interval = setInterval(atualizarpessoas, 1000);
 }
 function manterMSG(){//este puxa a estou vivo
     idinterval = setInterval(carregarMsg, 3000);
 }
-
 function estouVivo(){ //este puxa a carregarMSG
     
     const promisse2 = axios.post("https://mock-api.driven.com.br/api/v4/uol/status", {name:nome})
     promisse2.catch(tratarFalha)
     
 }
-
 function carregarMsg(){//este puxa a exibir msg
     messages=axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
     messages.then(postarMsg)
     messages.catch(tratarFalha)
     //exibirMsgs()
 }   
-
 function postarMsg(resposta){
     dados_msg=resposta.data;
     
@@ -127,35 +113,45 @@ function postarMsg(resposta){
     
     if((dados_msg[99].time)!=((dados_msg2[99].time))){ //se tem atualização nas mensagens
         console.log("tem atualização")
+        console.log(dados_msg[99])
+
         dados_msg2=dados_msg;
         msgHTML.innerHTML =""
-
-    
         for(var i = 0;i<(dados_msg.length);i++){
         if(dados_msg[i].type =="status"){
             msgHTML.innerHTML +=
-    
             `
             <li class="message-text status ">
                 <span class="hour">(${dados_msg[i].time})</span>
                 <span class="person-name">${dados_msg[i].from }</span>
                 <span>${dados_msg[i].text }</span>
             </li>
-           
-        
             `
         }
     
-        else{
+        else if(dados_msg[i].to =="Todos"){
             msgHTML.innerHTML +=
             `
-            <li class="message-text data-identifier="message"">
+            <li class="message-text" data-identifier="message">
                 <span class="hour">(${dados_msg[i].time})</span>
                 <span class="person-name">${dados_msg[i].from }</span>
                 <span>${dados_msg[i].text }</span>
             </li>
             `
         }
+        else if(dados_msg[i].to ==nome){
+            msgHTML.innerHTML +=
+            `
+            <li class="message-text private" data-identifier="message"">
+                <span class="hour">(${dados_msg[i].time})</span>
+                <span class="person-name">${dados_msg[i].from}</span>
+                <p> reservadamente para  <span> </span> </p>
+                <span class="person-name"> ${dados_msg[i].to} </span>
+                <span>${dados_msg[i].text}</span>
+            </li>
+            `
+        }
+
         }
         let elementoQueQueroQueApareca = document.querySelector("ul li:last-child");
         elementoQueQueroQueApareca.scrollIntoView();
@@ -166,7 +162,6 @@ function postarMsg(resposta){
     }
 
 }
-
 function exibeLateral(){
     let menulateral = document.querySelector("aside");
     menulateral.classList.toggle("escondido")
@@ -184,10 +179,30 @@ function pegarpessoas(resposta1){
     for(let i=0; i<(dados_persons.length);i++){
         documento_person.innerHTML+=`
         <li class="person-in-list">
-        <ion-icon name="person-circle"></ion-icon>
-        <p class="name-person-list">${dados_persons[i].name}</p>
+            <div class="left-content">
+                <ion-icon name="person-circle"></ion-icon>
+                <p class="name-person-list">${dados_persons[i].name}</p>
+            </div>
+        <ion-icon class = "escondido visible" name="checkmark"></ion-icon>
         </li>
         `;
+    }
+    
+}
+
+function selectionType(element){
+    
+    let type_message = document.querySelector(".icon-check.visible");
+    if(type_message!=null){
+        type_message.classList.remove("visible")
+    }
+    element.querySelector("ion-icon.icon-check").classList.add("visible")
+    if(element.className=="public"){
+        dados_type_message = "message"
+    }
+    else{
+        if(dados_nome_direct != "Todos"){        dados_type_message = "private_message"}
+        else{dados_type_message = "message"}
     }
     
 }
@@ -196,5 +211,13 @@ function fora(){
     menulateral.classList.add("escondido")
 }
 
+function atualizarpessoas(){
+    let promisseperson=axios.get("https://mock-api.driven.com.br/api/v4/uol/participants");
+    promisseperson.then(pegarpessoas)
+    promisseperson.catch(tratarFalha)
+}
 
 
+function selectionPerson(elemento){
+    let person_div= querySelector()
+}
